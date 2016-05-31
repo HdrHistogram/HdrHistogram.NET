848551278371f100e2c6f0b22322dfad3989b1a8
Lee Campbell <lee.ryan.campbell@gmail.com>
2016-05-31 22:34:44 +0800

    Benchmarking LeadingZeroCount performance

---
+++ src/HdrHistogram.Benchmarking/Program.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Running;
+using HdrHistogram.Benchmarking.LeadingZeroCount;
+
+namespace HdrHistogram.Benchmarking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var switcher = new BenchmarkSwitcher(new[] {
+                typeof(LeadingZeroCount64BitBenchmark),
+                typeof(LeadingZeroCount32BitBenchmark)
+            });
+            switcher.Run(args);
+        }
+    }
+}
