17efb0d2fc7735676c69ff15205bcf2e97fe9898
Lee Campbell <lee.ryan.campbell@gmail.com>
2016-06-02 22:04:06 +0800

    Minimizing supported benchmark platform/frameworks

--- src/HdrHistogram.Benchmarking/ExhuastiveJobWithMemoryDiagnosisConfig.cs
+++ src/HdrHistogram.Benchmarking/ExhuastiveJobWithMemoryDiagnosisConfig.cs
@@ -12,9 +12,9 @@ namespace HdrHistogram.Benchmarking
             Add(new MemoryDiagnoser());
 
             var crossProduct = from jitter in new[] {Jit.LegacyJit, Jit.RyuJit}
-                               from platform in new[] {Platform.AnyCpu, Platform.X64, Platform.X86}
+                               from platform in new[] {/*Platform.AnyCpu,*/ Platform.X64, Platform.X86}
                                //Lets not test old/unsupported frameworks.
-                               from framework in new[] { /*Framework.V40, Framework.V45, Framework.V451,*/ Framework.V452, /*Framework.V46, Framework.V461,*/ Framework.V462 } 
+                               from framework in new[] { /*Framework.V40, Framework.V45, Framework.V451, Framework.V452, Framework.V46, Framework.V461,*/ Framework.V462 } 
                                //from toolchain in new[] {Toolchain.}
                                //Currently the project doesn't support Core or Mono
                                from runtime in new[] {Runtime.Clr, /*Runtime.Core , Runtime.Mono*/}
