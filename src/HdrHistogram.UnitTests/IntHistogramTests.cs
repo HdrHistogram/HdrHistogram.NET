b72d4414e019d76d7e788e06a152047b64668ddf
Lee Campbell <lee.ryan.campbell@gmail.com>
2016-06-16 09:00:28 +0800

    Unit test for Copy & RecordValueWithCount

--- src/HdrHistogram.UnitTests/IntHistogramTests.cs
+++ src/HdrHistogram.UnitTests/IntHistogramTests.cs
@@ -16,18 +16,5 @@ namespace HdrHistogram.UnitTests
         {
             return new IntHistogram(lowestTrackableValue, highestTrackableValue, numberOfSignificantValueDigits);
         }
-        
-        [Test]
-        public void Can_add_LongHistogram_with_values_in_range()
-        {
-            var longHistogram = new LongHistogram(int.MaxValue - 1, 3);
-            longHistogram.RecordValueWithCount(1, 100);
-            longHistogram.RecordValueWithCount(int.MaxValue - 1, 1000);
-
-            var shortHistogram = new ShortHistogram(int.MaxValue - 1, 3);
-            shortHistogram.Add(longHistogram);
-
-            HistogramAssert.AreValueEqual(longHistogram, shortHistogram);
-        }
     }
 }
--- src/HdrHistogram.UnitTests/ShortHistogramTests.cs
+++ src/HdrHistogram.UnitTests/ShortHistogramTests.cs
@@ -16,30 +16,5 @@ namespace HdrHistogram.UnitTests
         {
             return new ShortHistogram(lowestTrackableValue, highestTrackableValue, numberOfSignificantValueDigits);
         }
-
-        [Test]
-        public void Can_add_IntHistogram_with_values_in_range()
-        {
-            var intHistogram = new IntHistogram(short.MaxValue-1, 3);
-            intHistogram.RecordValueWithCount(1, 100);
-            intHistogram.RecordValueWithCount(short.MaxValue-1, 1000);
-
-            var shortHistogram = new ShortHistogram(short.MaxValue-1, 3);
-            shortHistogram.Add(intHistogram);
-
-            HistogramAssert.AreValueEqual(intHistogram, shortHistogram);
-        }
-        [Test]
-        public void Can_add_LongHistogram_with_values_in_range()
-        {
-            var longHistogram = new LongHistogram(short.MaxValue-1, 3);
-            longHistogram.RecordValueWithCount(1, 100);
-            longHistogram.RecordValueWithCount(short.MaxValue-1, 1000);
-
-            var shortHistogram = new ShortHistogram(short.MaxValue-1, 3);
-            shortHistogram.Add(longHistogram);
-
-            HistogramAssert.AreValueEqual(longHistogram, shortHistogram);
-        }
     }
 }
