899c9f7906c51c530f1d405d531c368a4f65ff32
Lee Campbell <lee.ryan.campbell@gmail.com>
2016-06-17 09:01:58 +0800

    Adding Concurrent benchmarks

--- src/HdrHistogram.UnitTests/Recording/RecorderTestWithIntConcurrentHistogram.cs
+++ src/HdrHistogram.UnitTests/Recording/RecorderTestWithIntConcurrentHistogram.cs
@@ -3,11 +3,11 @@ using NUnit.Framework;
 namespace HdrHistogram.UnitTests.Recording
 {
     [TestFixture]
-    public sealed class RecorderTestWithConcurrentHistogram : RecorderTestsBase
+    public sealed class RecorderTestWithIntConcurrentHistogram : RecorderTestsBase
     {
         protected override HistogramBase Create(long id, long min, long max, int sf)
         {
-            return new LongConcurrentHistogram(id, min, max, sf);
+            return new IntConcurrentHistogram(id, min, max, sf);
         }
     }
 }
