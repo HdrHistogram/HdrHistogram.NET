5abd0cfaeb3980483f9d5a906d9accb30c8153d7
Allan hardy <al_hardy@live.com.au>
2017-01-28 22:49:21 +1100

    #17 updated exhuastive job with latest benchmarkdotnet api

--- src/HdrHistogram.Benchmarking/ExhuastiveJobWithMemoryDiagnosisConfig.cs
+++ src/HdrHistogram.Benchmarking/ExhuastiveJobWithMemoryDiagnosisConfig.cs
@@ -1,6 +1,7 @@
-using System.Linq;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
 
 namespace HdrHistogram.Benchmarking
 {
@@ -10,23 +11,14 @@ namespace HdrHistogram.Benchmarking
         {
             Add(new MemoryDiagnoser());
 
-            //var crossProduct = from jitter in new[] {Jit.LegacyJit, Jit.RyuJit}
-            //                   from platform in new[] {/*Platform.AnyCpu,*/ Platform.X64, Platform.X86}
-            //                   //Lets not test old/unsupported frameworks.
-            //                   from framework in new[] { /*Framework.V40, Framework.V45, Framework.V451, Framework.V452, Framework.V46, Framework.V461,*/ Framework.V462 } 
-            //                   //from toolchain in new[] {Toolchain.}
-            //                   //Currently the project doesn't support Core or Mono
-            //                   from runtime in new[] {Runtime.Clr, /*Runtime.Core , Runtime.Mono*/}
-            //                   //where !(runtime == Runtime.Core && (jitter == Jit.LegacyJit || platform!=Platform.X64))
-            //                   select new Job
-            //                   {
-            //                       Jit = jitter,
-            //                       Platform = platform,
-            //                       Framework = framework,
-            //                       Runtime = runtime
-            //                   };
-
-            //Add(crossProduct.ToArray());
+            Add(
+                Job.Dry
+                .With(Platform.X64)
+                .With(Platform.X86)
+                .With(Jit.RyuJit)
+                .With(Jit.LegacyJit)
+                .With(Runtime.Clr)
+                .WithId("ExhuastiveJobWithMemoryDiagnosis"));
         }
     }
 }
