ab6a0ef95862223df2d7c2b4309bc18661624463
Allan hardy <al_hardy@live.com.au>
2017-01-28 22:38:04 +1100

    #17 changing benchmarking project to xproj so it can reference core project

--- src/HdrHistogram.Benchmarking/ExhuastiveJobWithMemoryDiagnosisConfig.cs
+++ src/HdrHistogram.Benchmarking/ExhuastiveJobWithMemoryDiagnosisConfig.cs
@@ -1,7 +1,6 @@
 using System.Linq;
 using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Diagnostics.Windows;
-using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Diagnosers;
 
 namespace HdrHistogram.Benchmarking
 {
@@ -11,23 +10,23 @@ namespace HdrHistogram.Benchmarking
         {
             Add(new MemoryDiagnoser());
 
-            var crossProduct = from jitter in new[] {Jit.LegacyJit, Jit.RyuJit}
-                               from platform in new[] {/*Platform.AnyCpu,*/ Platform.X64, Platform.X86}
-                               //Lets not test old/unsupported frameworks.
-                               from framework in new[] { /*Framework.V40, Framework.V45, Framework.V451, Framework.V452, Framework.V46, Framework.V461,*/ Framework.V462 } 
-                               //from toolchain in new[] {Toolchain.}
-                               //Currently the project doesn't support Core or Mono
-                               from runtime in new[] {Runtime.Clr, /*Runtime.Core , Runtime.Mono*/}
-                               //where !(runtime == Runtime.Core && (jitter == Jit.LegacyJit || platform!=Platform.X64))
-                               select new Job()
-                               {
-                                   Jit = jitter,
-                                   Platform = platform,
-                                   Framework = framework,
-                                   Runtime = runtime
-                               };
+            //var crossProduct = from jitter in new[] {Jit.LegacyJit, Jit.RyuJit}
+            //                   from platform in new[] {/*Platform.AnyCpu,*/ Platform.X64, Platform.X86}
+            //                   //Lets not test old/unsupported frameworks.
+            //                   from framework in new[] { /*Framework.V40, Framework.V45, Framework.V451, Framework.V452, Framework.V46, Framework.V461,*/ Framework.V462 } 
+            //                   //from toolchain in new[] {Toolchain.}
+            //                   //Currently the project doesn't support Core or Mono
+            //                   from runtime in new[] {Runtime.Clr, /*Runtime.Core , Runtime.Mono*/}
+            //                   //where !(runtime == Runtime.Core && (jitter == Jit.LegacyJit || platform!=Platform.X64))
+            //                   select new Job
+            //                   {
+            //                       Jit = jitter,
+            //                       Platform = platform,
+            //                       Framework = framework,
+            //                       Runtime = runtime
+            //                   };
 
-            Add(crossProduct.ToArray());
+            //Add(crossProduct.ToArray());
         }
     }
 }
