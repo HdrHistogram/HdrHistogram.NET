92f30d9ee707f3998b4e61e3f235ab4e6e79f472
Lee Campbell <lee.ryan.campbell@gmail.com>
2017-06-05 21:53:20 +0800

    Build.cmd corrections

--- build.cmd
+++ build.cmd
@@ -1,8 +1,15 @@
-SET SemVer="1.0.0.0-local"
+@ECHO OFF
 
-pushd %~dp0%..\
-	dotnet restore .\src\
-	dotnet build .\src\ -c=Release /p:Version=%SemVer%
-	dotnet test .\src\HdrHistogram.UnitTests\HdrHistogram.UnitTests.csproj /p:Configuration=Release
-	dotnet pack .\src\HdrHistogram\HdrHistogram.csproj -c=Release --include-symbols /p:Version=%SemVer%
-popd
+if [%1]==[] (
+ SET SemVer="1.0.0.0-local"
+) ELSE (
+ SET SemVer=%1
+)
+
+dotnet restore -v=q
+
+dotnet build -v=q -c=Release /p:Version=%SemVer%
+
+dotnet test .\HdrHistogram.UnitTests\HdrHistogram.UnitTests.csproj -v=q --no-build -c=Release
+
+dotnet pack .\HdrHistogram\HdrHistogram.csproj --no-build --include-symbols -c=Release /p:Version=%SemVer%
