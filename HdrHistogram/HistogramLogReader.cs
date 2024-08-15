c1a6c68cbcb0c0701124388fa7645bb4252203e1
Lee Campbell <lee.ryan.campbell@gmail.com>
2024-08-15 06:51:08 +0100

    Closing streams before returning them

--- HdrHistogram/HistogramLogReader.cs
+++ HdrHistogram/HistogramLogReader.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 using HdrHistogram.Utilities;
 
@@ -29,7 +30,8 @@ namespace HdrHistogram
         {
             using (var reader = new HistogramLogReader(inputStream))
             {
-                return reader.ReadHistograms();
+                return reader.ReadHistograms()
+                    .ToArray();
             }
         }
 
